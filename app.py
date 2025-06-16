8d57c0b9d7ab915ccdb30dbd72f69ac017f9a3f2
Muhammad Uzair Shafiq <uzairshafiq473@gmail.com>
2025-06-16 10:24:15 +0500

    push

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ def chat():
 
 # Add this at the end
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 else:
     # For Vercel deployment
     application = app
